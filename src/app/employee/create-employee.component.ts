9fced48a7e779ebaa42e8addc7c304f5108565af
bil <bil092014@gmail.com>
2020-01-01 17:31:42 -0500

    photo preview

--- src/app/employee/create-employee.component.ts
+++ src/app/employee/create-employee.component.ts
@@ -9,23 +9,23 @@ import {BsDatepickerConfig} from 'ngx-bootstrap/datepicker';
   styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent implements OnInit {
-  dateBirth = new Date(2019, 1, 1);
+  dateBirth = new Date(2018, 0, 1);
   // on besoin que d'une partie du BsDatepickerConfig => on utilise Parial
   // Lecture --> https://www.typescriptlang.org/docs/handbook/release-notes/typescript-2-1.html
   myDatepickerConfiger: Partial<BsDatepickerConfig>;
   departements: Departement[];
   defaultDepartement: number;
+  previewPhoto = false;
+  nameEmployeeToPreview: string;
 
   constructor() { }
 
   ngOnInit() {
     this.myDatepickerConfiger = {...{containerClass: 'theme-dark-blue',
-                                      showWeekNumbers : false,
-                                      dateInputFormat: 'DD/YY/YYYY',
-                                      minDate: new Date(2018, 1, 1),
-                                      maxDate: new Date(2019, 12, 31)
-                                    }
-                                };
+    showWeekNumbers : false,
+    dateInputFormat: 'DD/YY/YYYY',
+    minDate: new Date(2018, 0, 1),
+    maxDate: new Date(2019, 11, 31)}};
 
     this.departements = [
       { id: 1, name: 'IT' },
@@ -34,6 +34,14 @@ export class CreateEmployeeComponent implements OnInit {
       { id: 4, name: 'Help desk' }
     ];
     this.defaultDepartement = 2;
+  }
+
+  pathEmployeeToPreview(): string {
+    return `assets/images/${this.nameEmployeeToPreview}.png`;
+  }
+
+  previewPhotoToggle(): void {
+    this.previewPhoto = !this.previewPhoto;
   }
 
   saveEmployee(employeeForm: NgForm): void {
