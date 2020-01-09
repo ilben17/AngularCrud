ae632929ae9cdbbf9350573b4f557c92aa8ba1ab
bil <bil092014@gmail.com>
2020-01-09 14:55:47 -0500

    end validation

--- src/app/shared/directives/compare-match-validator.directive.ts
+++ src/app/shared/directives/compare-match-validator.directive.ts
@@ -1,16 +1,15 @@
-import {Directive, Input} from '@angular/core';
+import {Directive} from '@angular/core';
 import { Validator, AbstractControl, NG_VALIDATORS } from '@angular/forms';
-import { InvokeFunctionExpr } from '@angular/compiler';
 
 @Directive({
     selector: '[appComparMatching]',
     providers: [{provide: NG_VALIDATORS, useExisting: CompareControlsMatchingValidator, multi: true}]
 })
 export class CompareControlsMatchingValidator implements Validator {
-    @Input('appComparMatching') refControlToCompare: string;
-    validate(control: AbstractControl): {[key: string]: any} | null {
-        const contenuControlToCompare = control.parent.get(this.refControlToCompare);
-        if (contenuControlToCompare && contenuControlToCompare.value !== control.value) {
+    validate(passwordGroup: AbstractControl): {[key: string]: any} | null {
+        const passwordControl = passwordGroup.get('passwordName');
+        const confirmPasswordControl = passwordGroup.get('passwordConfirm');
+        if (passwordControl && confirmPasswordControl && passwordControl.value !== confirmPasswordControl.value) {
             return {'dontMatches': true};
         }
         return null;
